50f2f83247110d23e05f301ac58af8cffdd237a2
ShahidKhan232 <shahidkhan.95173@gmail.com>
2026-02-04 22:43:08 +0530

    formating the output of ai

--- server/src/routes/testAPI.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { testAPI } from '../controllers/testAPI.controller.js';
-
-const router = Router();
-
-// Test API endpoint
-router.get('/test', testAPI);
-
-export default router;
